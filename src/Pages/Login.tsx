af39af9b0d620cf1a33f4078c2adee3f33367e37
MiroMuro <miro.laukkanen44@gmail.com>
2024-12-03 23:09:07 +0200

    Refurnished the login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,12 +1,44 @@
+import padlock from "../assets/padlock(2).png";
+import user from "../assets/user.png";
 const Login = () => {
   return (
     <div className="w-full h-full flex justify-center items-center ">
       <form
-        className="w-1/3 h-2/5 flex flex-col justify-evenly bg-battleship-blue-light border-4 border-black rounded-xl
+        className="w-1/3 h-3/5 flex flex-col justify-evenly bg-battleship-blue-light border-4 border-black rounded-xl
     margin-x-auto text-white p-2"
       >
-        <div className="flex h-1/3 w-full"> Username</div>
-        <div className="flex h-1/3 w-full"> Password</div>
+        <header className="flex flex-col h-1/3 w-full">
+          <h2 className="text-center text-slate-400 italic text-xl">
+            It's now or never
+          </h2>
+          <h1 className="text-center  text-inherit text-3xl">
+            Login to play, captain!
+          </h1>
+        </header>
+        <div className="flex flex-row items-center h-1/3 w-full">
+          {" "}
+          <img
+            src={user}
+            className="border-2 h-8 w-8 bg-white border-white rounded-xl text-center mx-2 object-cover"
+          />
+          <input
+            placeholder="Username"
+            type="text"
+            className="bg-none outline-none p-5 rounded-xl text-black h-1/2"
+          />{" "}
+        </div>
+        <div className="flex flex-row items-center h-1/3 w-full">
+          {" "}
+          <img
+            src={padlock}
+            className="border-2 h-8 w-8 bg-white border-white rounded-xl text-center mx-2 object-cover"
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            className="bg-none outline-none p-5 rounded-xl text-black h-1/2"
+          />{" "}
+        </div>
         <div className="flex h-1/3 w-full justify-center items-center">
           <div className="flex-1 flex flex-col  h-5/6 bg-battleship-blue-light justify-end my-auto border-2 cursor-pointer rounded-xl mx-2  border-gray-400  shadow-xl transition transformation ease-in-out 200ms active:scale-75 hover:scale-105">
             <div className="h-3/4 w-full bg-battleship-blue rounded-xl p-1 relative">
